0418cefc9dabe8e515cf8e473cc77220e26902bb
AmrHE <amr.hassan.emam@gmail.com>
2022-01-25 14:32:49 +0200

    Finish Product.js component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,28 +1,43 @@
+import { useState } from "react";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
 
-const Product = ({
-	// id,
-	// title,
-	// price,
-	// description,
-	// category,
-	// image,
-	// rating,
-	product,
-}) => {
+const Product = ({ product }) => {
 	const { id, title, price, description, category, image, rating } = product;
+	const [isPrime, setIsPrime] = useState(Math.random() < 0.5 ? true : false);
 
 	return (
-		<div>
-			<p>{category}</p>
+		<div className="relative flex flex-col m-5 bg-white z-30 p-10">
+			<p className="absolute top-2 right-2	text-xs italic text-gray-400">
+				{category}
+			</p>
 
 			<Image src={image} height={200} width={200} objectFit="contain" />
-			<h4>{title}</h4>
+			<h4 className="my-3">{title}</h4>
+			{/* <p>{rating.rate}</p> */}
 
-			<div>
-				<StarIcon className="h-5 text-yellow-400" />
+			<div className="flex">
+				{Array(Math.round(rating.rate))
+					.fill()
+					.map((_, i) => (
+						<StarIcon key={i} className="h-5 text-yellow-500" />
+					))}
 			</div>
+
+			<p className="text-xs my-2 line-clamp-2">{description}</p>
+
+			<div className="mb-5">
+				<Currency quantity={price} currency="USD" />
+			</div>
+
+			{isPrime && (
+				<div className="flex items-center space-x-2 -mt-5">
+					<img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+					<p className="text-xs text-gray-500">FREE Next-dat Delivery</p>
+				</div>
+			)}
+			<button className="mt-auto button">Add to Cart</button>
 		</div>
 	);
 };
