6e3592487b311b211e1eff41e60cdaaa5eb70a0f
AmrHE <amr.hassan.emam@gmail.com>
2022-01-26 17:20:53 +0200

    add new redux actions

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -1,16 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  items: [],
+	items: [],
 };
 
 export const basketSlice = createSlice({
-  name: "basket",
-  initialState,
-  reducers: {
-    addToBasket: (state, action) => {},
-    removeFromBasket: (state, action) => {},
-  },
+	name: "basket",
+	initialState,
+	reducers: {
+		//ACTIONS
+		addToBasket: (state, action) => {
+			state.items = [...state.items, action.payload];
+		},
+		removeFromBasket: (state, action) => {
+			const index = state.items.findIndex(
+				(item) => item.id === action.payload.id
+			);
+
+			let newBasket = [...state.items];
+
+			if (index >= 0) {
+				newBasket.splice(index, 1);
+			} else {
+				console.warn(
+					`Can't remove product (id: ${action.payload.id}) as it's not in the Cart`
+				);
+			}
+
+			state.items = newBasket;
+		},
+
+		//TODO ADD NEW ACTIONS FOR QUANTITY (INCREASE QUANTITY, DECREASE QUANTITY)
+	},
 });
 
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
