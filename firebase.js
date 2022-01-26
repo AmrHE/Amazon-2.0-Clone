37cfdff384759388553c7d65926a9726a1dac072
AmrHE <amr.hassan.emam@gmail.com>
2022-01-26 17:16:20 +0200

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyA8o4wZjUMpQfesANuGA954pls1ZyxwiuY",
+	authDomain: "clone-2ad09.firebaseapp.com",
+	projectId: "clone-2ad09",
+	storageBucket: "clone-2ad09.appspot.com",
+	messagingSenderId: "633549944996",
+	appId: "1:633549944996:web:219c285dcb5763704c8e31",
+};
