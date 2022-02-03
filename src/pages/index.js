2cb20d71e200e3a77d7abebdb9e750fd94e588ec
AmrHE <amr.hassan.emam@gmail.com>
2022-02-03 13:53:44 +0200

    loading bug fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
@@ -25,6 +26,7 @@ export default function Home({ products }) {
 }
 
 export async function getServerSideProps(context) {
+	const session = await getSession(context);
 	const products = await fetch("https://fakestoreapi.com/products").then(
 		(res) => res.json()
 	);
@@ -32,6 +34,7 @@ export async function getServerSideProps(context) {
 	return {
 		props: {
 			products,
+			session,
 		},
 	};
 }
