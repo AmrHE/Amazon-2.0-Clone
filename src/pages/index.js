cf8d93fc4cf7cf04437704bd01df3a2ee9b63970
AmrHE <amr.hassan.emam@gmail.com>
2022-02-07 18:17:00 +0200

    edit / bug fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,19 @@ import { getSession } from "next-auth/react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
+import { addProducts } from "../slices/basketSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Footer from "../components/Footer";
+import BackToTop from "../components/BackToTop";
 
 export default function Home({ products }) {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(addProducts(products));
+	}, [products]);
+
 	return (
 		<div className="bg-gray-100">
 			<Head>
@@ -12,7 +23,7 @@ export default function Home({ products }) {
 			</Head>
 
 			{/* HEADER SECTION */}
-			<Header />
+			<Header products={products} />
 
 			{/* BANNER + PRODUCT FEED (MAIN VIEW) */}
 			<main className="max-w-screen-2xl mx-auto">
@@ -21,6 +32,8 @@ export default function Home({ products }) {
 				{/* PRODUCT FEED */}
 				<ProductFeed products={products} />
 			</main>
+			<BackToTop />
+			<Footer />
 		</div>
 	);
 }
