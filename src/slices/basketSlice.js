b8c44495fa530c40ce6fa2edd9b671efc6574188
AmrHE <amr.hassan.emam@gmail.com>
2022-02-07 18:15:06 +0200

    add new actions, reducers & selectors

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -2,6 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
 	items: [],
+	products: null,
+	filteredProducts: null,
 };
 
 export const basketSlice = createSlice({
@@ -9,8 +11,38 @@ export const basketSlice = createSlice({
 	initialState,
 	reducers: {
 		//ACTIONS
+		addProducts: (state, action) => {
+			state.products = action.payload;
+			state.filteredProducts = action.payload;
+		},
 		addToBasket: (state, action) => {
-			state.items = [...state.items, action.payload];
+			const index = state.items.findIndex(
+				(item) => item.id == action.payload.id
+			);
+			if (action.payload.quantity > 0) {
+				if (index >= 0) {
+					state.items[index].quantity += action.payload.quantity;
+				} else {
+					state.items = [...state.items, action.payload];
+				}
+			}
+		},
+		updateQuantity: (state, action) => {
+			const index = state.items.findIndex(
+				(item) => item.id == action.payload.id
+			);
+			if (index >= 0) {
+				if (action.payload.quantity > 0) {
+					state.items[index].quantity = action.payload.quantity;
+				} else {
+					let newBasket = [...state.items];
+					newBasket.splice(index, 1);
+					state.items = newBasket;
+				}
+			} else
+				console.log(
+					`Can't remove product ${action.payload.id} as its does not exist!`
+				);
 		},
 		removeFromBasket: (state, action) => {
 			const index = state.items.findIndex(
@@ -29,16 +61,34 @@ export const basketSlice = createSlice({
 
 			state.items = newBasket;
 		},
-
-		//TODO ADD NEW ACTIONS FOR QUANTITY (INCREASE QUANTITY, DECREASE QUANTITY)
+		updateFilters: (state, action) => {
+			state.filteredProducts = action.payload;
+		},
+		clearFilters: (state) => {
+			state.filteredProducts = state.products;
+		},
 	},
 });
 
-export const { addToBasket, removeFromBasket } = basketSlice.actions;
+export const {
+	addToBasket,
+	removeFromBasket,
+	updateQuantity,
+	addProducts,
+	updateFilters,
+	clearFilters,
+} = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
+export const selectProducts = (state) => state.basket.products;
 export const selectItems = (state) => state.basket.items;
-export const SelectTotal = (state) =>
-	state.basket.items.reduce((total, item) => total + item.price, 0);
+export const selectTotal = (state) =>
+	state.basket.items.reduce(
+		(total, item) => total + item.price * item.quantity,
+		0
+	);
+export const selectTotalItems = (state) =>
+	state.basket.items.reduce((total, item) => total + item.quantity, 0);
+export const selectFilteredProducts = (state) => state.basket.filteredProducts;
 
 export default basketSlice.reducer;
