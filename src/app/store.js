fcc31bb1bf1f2f8bf1d73f619b9d38bcb5f4a3a9
AmrHE <amr.hassan.emam@gmail.com>
2022-02-07 19:15:46 +0200

    edit

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
-//THE GLOBAL STORE SETUP
+// THE GLOBAL STORE SETUP REDUX/TOOLKIT
 export const store = configureStore({
 	reducer: {
 		basket: basketReducer,
